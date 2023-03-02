716a2743b4d66ed7a31f17322055630acbf82a6b
salimovaruzila02 <salimovaruzila02@gmail.com>
2023-03-02 12:56:02 +0300

    auto

--- workshop/apps.py
+++ workshop/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class WorkshopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    verbose_name = 'АПИШЕЧКА'
-    name = 'api'
+    name = 'workshop'
