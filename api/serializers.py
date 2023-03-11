365396118c8f54346400f9bac8a03a393c889d6e
salimovaruzila02 <salimovaruzila02@gmail.com>
2023-03-11 22:35:35 +0300

    fix serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,44 @@
 from rest_framework import serializers
 from .models import Product, Category, Manufacturer
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'size', 'manufacturer', 'category', 'price')
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, allow_blank=False, max_length=100)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    manufacturer = serializers.PrimaryKeyRelatedField(queryset=Manufacturer.objects.all())
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'name')
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
 
-class ManufacturerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Manufacturer
-        fields = ('id', 'name', 'country')
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.category = validated_data.get('category', instance.category)
+        instance.manufacturer = validated_data.get('manufacturer', instance.manufacturer)
+        instance.save()
+        return instance
+
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, allow_blank=False, max_length=100)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+class ManufacturerSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, allow_blank=False, max_length=100)
+
+    def create(self, validated_data):
+        return Manufacturer.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
